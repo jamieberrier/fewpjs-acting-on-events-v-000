b3a03668fab9caa5cc777d42d7cd3c97e06ab965
Jamie Berrier <berrier.jamie@gmail.com>
2020-05-28 15:25:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-  let leftNumbers = dodger.style.left.replace("px", "");
-  let left = parseInt(leftNumbers, 10);
+  let rightNumbers = dodger.style.left.replace("px", "");
+  let right = parseInt(rightNumbers, 10);
 
   if (left > 0) {
     dodger.style.left = `${left - 1}px`;
